8820f9539edc69861579b12e8bcfc3dce495e2ca
smsohan <ThoughtWorks@SM6631-MacBook-Pro.local>
2011-02-07 15:23:50 -0700

    fixed a failing test code

--- spec/models/mv_salesforce_object_spec.rb
+++ spec/models/mv_salesforce_object_spec.rb
@@ -7,8 +7,7 @@ describe MvSalesforceObject do
       mv_salesforce_object = MvSalesforceObject.new
       it "should call lookup for school id" do
         SalesforceContact.should_receive(:get_first_or_create).with('A Monitor').and_return("2")
-        mv_salesforce_object.should_receive(:get_first_record).with(:Id, :School__c, "name='School A'")
-          .and_return("1")
+        MvSalesforceObject.should_receive(:get_first_record).with(:Id, :School__c, "name='School A'").and_return("1")
         SalesforceContact.should_receive(:get_first_or_create).with('A TM').and_return("3")
 
         mv_salesforce_object.field_values = {:School__c => 'School A', :Monitor__c => "A Monitor", :TM__c =>'A TM'}
