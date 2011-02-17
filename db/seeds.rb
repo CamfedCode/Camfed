e2b952106f5b130499b92918d97aa08a3397022e
Tyler Mercier <tylermercier@gmail.com>
2011-02-17 09:13:20 -0700

    removed mappings and tokens from db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@ if Configuration.count == 0
   Configuration.create!({
       :epi_surveyor_url => 'https://www.episurveyor.org', 
       :epi_surveyor_user => 'Camfedtest@gmail.com',
-      :epi_surveyor_token => 'YUc8UfyeOm3W9GqNSJYs',
+      :epi_surveyor_token => 'replace with token',
       :salesforce_url => 'https://test.salesforce.com/services/Soap/u/20.0',
       :salesforce_user => 'sf_sysadmin@camfed.org.dean',
-      :salesforce_token => 'w3stbrookLidRts9sALeXQYTYhYJUvl5wc', 
+      :salesforce_token => 'replace with token', 
      })
 end
 
