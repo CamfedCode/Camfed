7aebf33ac2f7cc0f820c6b083371b1c7feb7bcd5
Mark Ryall <mark@ryall.name>
2012-03-02 15:43:04 +1000

    patched gem to allow content to be html escaped

--- vendor/gems/best_in_place-0.2.3/lib/best_in_place/helper.rb
+++ vendor/gems/best_in_place-0.2.3/lib/best_in_place/helper.rb
@@ -34,6 +34,8 @@ module BestInPlace
       if !opts[:sanitize].nil? && !opts[:sanitize]
         out << " data-sanitize='false'>"
         out << sanitize(value.to_s, :tags => %w(b i u s a strong em p h1 h2 h3 h4 h5 ul li ol hr pre span img br), :attributes => %w(id class href))
+      elsif opts[:escape]
+        out << ">#{h value.to_s}"
       else
         out << ">#{sanitize(value.to_s, :tags => nil, :attributes => nil)}"
       end
