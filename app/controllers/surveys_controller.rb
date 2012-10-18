481048978c229a5fc79cc7e867b5a05081dfc84f
Shaheedha <shaheedha@gmail.com>
2012-10-18 11:15:40 +0530

    Pagination done for search results too

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -65,7 +65,7 @@ class SurveysController < AuthenticatedController
   end
   
   def search
-    @surveys = EpiSurveyor::Survey.where("LOWER(surveys.name) LIKE ?", "%#{params[:query].downcase}%")
+    @surveys = EpiSurveyor::Survey.where("LOWER(surveys.name) LIKE ?", "%#{params[:query].downcase}%").paginate(:page => params[:page]).order('name ASC')
     add_crumb 'Surveys', surveys_path
     add_crumb "Search results for: #{params[:query]}"
     render :index
