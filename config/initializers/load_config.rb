692108fa65c2371c8b00f0bfa002ccbfb58426a8
Ankit Dhingra <ankitsdhingra@gmail.com>
2013-02-08 13:20:00 +0530

    <Ankit> Fixing the way we are loading twilio properties for production

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,2 +1,2 @@
 APP_CONSTANTS = YAML.load_file("#{Rails.root}/config/app_constants.yml")
-TWILIO_CONFIG = YAML.load_file("#{Rails.root}/config/twilio.yml")
+TWILIO_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/twilio.yml")).result)
